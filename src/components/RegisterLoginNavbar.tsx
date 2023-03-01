1e40496e6c4a98605104ebaee839e3db400e3be2
Darkelfs56 <75739105+darkelfs56@users.noreply.github.com>
2023-03-02 05:52:01 +0800

    Added RegisterLoginNavbar.

---
+++ src/components/RegisterLoginNavbar.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface RegisterLoginNavbarProps {}
+
+const RegisterLoginNavbar: FC<RegisterLoginNavbarProps> = () => {
+  return (
+    <>
+      <div>This is a navbar</div>
+    </>
+  );
+};
+
+export default RegisterLoginNavbar;
