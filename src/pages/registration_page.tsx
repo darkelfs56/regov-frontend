ca354deb35e3aeab83061789ee45a4ca47d88f6f
Darkelfs56 <75739105+darkelfs56@users.noreply.github.com>
2023-03-02 08:43:52 +0800

    Finished UI for registration form.

--- src/pages/registration_page.tsx
+++ src/pages/registration_page.tsx
@@ -2,6 +2,10 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import RegisterLoginNavbar from "~/components/RegisterLoginNavbar";
+import { IoIosArrowDown } from "react-icons/io";
+
+const buttonStyle =
+  "flex cursor-pointer items-center gap-[20px] border-2 border-solid border-black p-2 bg-white rounded-md";
 
 const RegistrationPage: NextPage = () => {
   return (
@@ -15,7 +19,104 @@ const RegistrationPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <RegisterLoginNavbar routeName={"register"} />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"></main>
+      <main className="flex min-h-screen flex-col items-center bg-[#F7F9FB]">
+        <div className="mt-10">
+          <div className="p-4">
+            <form action="" className="flex flex-col">
+              <div className="text-center text-[20px] font-bold">
+                Register new account
+              </div>
+              <div className="mt-10 flex flex-col">
+                <div className="flex flex-col gap-[10px]">
+                  <label htmlFor="email" className="font-medium">
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    className="h-[45px] w-[363px] rounded-md"
+                  />
+                </div>
+                <div className="mt-[10px] flex flex-col gap-[10px]">
+                  <label htmlFor="username" className="font-medium">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    className="h-[45px] w-[363px] rounded-md"
+                  />
+                </div>
+                <div className="mt-[10px] flex flex-col gap-[10px]">
+                  <label htmlFor="password" className="font-medium">
+                    Password
+                  </label>
+                  <input
+                    type="text"
+                    name="password"
+                    className="h-[45px] w-[363px] rounded-md"
+                  />
+                </div>
+                <div className="mt-[10px] flex flex-col gap-[10px]">
+                  <label htmlFor="re-enter_password" className="font-medium">
+                    Re-enter password
+                  </label>
+                  <input
+                    type="text"
+                    name="re-enter_password"
+                    className="h-[45px] w-[363px] rounded-md"
+                  />
+                </div>
+                <div className="mt-[10px] flex flex-col gap-[10px]">
+                  <label htmlFor="dateofbirth" className="font-medium">
+                    Date of birth
+                  </label>
+                  <div className="flex w-[363px] justify-between">
+                    <button className={buttonStyle}>
+                      Day <IoIosArrowDown />
+                    </button>
+                    <button className={buttonStyle}>
+                      Month <IoIosArrowDown />
+                    </button>
+                    <button className={buttonStyle}>
+                      Year <IoIosArrowDown />
+                    </button>
+                  </div>
+                  <div className="mt-12 flex flex-col">
+                    <button className="w-[363px] cursor-pointer items-center justify-center gap-[20px] rounded-md border-2 border-solid border-[#00C649] bg-[#00C649] p-2 text-center text-white">
+                      Continue
+                    </button>
+                    <Link
+                      href={""}
+                      className="mt-[10px] text-[16px] font-medium text-[#3E51FA]"
+                    >
+                      Already have an account?
+                    </Link>
+                    <p className="mt-[11px] text-[12px]">
+                      By registering, you agree to the
+                      <Link
+                        href={""}
+                        className="text-[12px] font-medium text-[#3E51FA]"
+                      >
+                        {" "}
+                        Terms of Service
+                      </Link>{" "}
+                      and
+                      <Link
+                        href={""}
+                        className="text-[12px] font-medium text-[#3E51FA]"
+                      >
+                        {" "}
+                        Privacy Policy
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
